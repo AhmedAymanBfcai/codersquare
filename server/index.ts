2eed422b1bf2430d50f7a975460027e4b4e191d6
Ahmed Ayman <69440618+AhmedAymanBfcai@users.noreply.github.com>
2022-02-07 20:18:26 +0200

    added some stuff

--- server/index.ts
+++ server/index.ts
@@ -1,1 +1,18 @@
-console.log('From Index.ts');
+import { createPostHandler, listPostsHandler } from './handlers/postHandler';
+import express, { RequestHandler } from 'express';
+
+const app = express();
+
+app.use(express.json());
+
+const requestLoggerMiddleware: RequestHandler = (req, res, next) => {
+  console.log(req.method, req.path, '- body:', req.body);
+  next();
+};
+
+app.use(requestLoggerMiddleware);
+
+app.get('/posts', listPostsHandler);
+app.post('/posts', createPostHandler);
+
+app.listen(3000);
