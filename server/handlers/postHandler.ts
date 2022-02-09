11b9610270a0a19d0e22c306982eaef4bca341e2
Ahmed Ayman <69440618+AhmedAymanBfcai@users.noreply.github.com>
2022-02-09 13:44:07 +0200

    prettier, more api types

--- server/handlers/postHandler.ts
+++ server/handlers/postHandler.ts
@@ -1,23 +1,30 @@
-import { db } from '../datastore';
-import { ExpressHandler, Post } from '../types';
 import crypto from 'crypto';
 
-export const listPostsHandler: ExpressHandler<{}, {}> = (request, response) => {
-  response.send({ posts: db.listPosts() });
+import { CreatePostRequest, CreatePostResponse, ListPostsRequest, ListPostsResponse } from '../api';
+import { db } from '../datastore';
+import { ExpressHandler, Post } from '../types';
+
+export const listPostsHandler: ExpressHandler<ListPostsRequest, ListPostsResponse> = (
+  request,
+  response
+) => {
+  // TODO: add pagination and filtering
+  return response.send({ posts: db.listPosts() });
 };
 
-type CreatePostRequest = Pick<Post, 'title' | 'url' | 'userId'>;
-
-interface CreatePostResponse {}
-
-export const createPostHandler: ExpressHandler<
-  CreatePostRequest,
-  CreatePostResponse
-> = (req, res) => {
+export const createPostHandler: ExpressHandler<CreatePostRequest, CreatePostResponse> = (
+  req,
+  res
+) => {
+  // TODO: better error messages
   if (!req.body.title || !req.body.url || !req.body.userId) {
     return res.sendStatus(400);
   }
 
+  // TODO: validate user exists
+  // TODO: get user Id from session
+  // TODO: validate title and url are non-empty
+  // TODO: validate url is new, otherwise add +1 to existing post
   const post: Post = {
     id: crypto.randomUUID(),
     postedAt: Date.now(),
@@ -26,5 +33,5 @@ export const createPostHandler: ExpressHandler<
     userId: req.body.userId,
   };
   db.createPost(post);
-  res.sendStatus(200);
+  return res.sendStatus(200);
 };
